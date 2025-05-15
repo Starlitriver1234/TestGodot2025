ac51437c72963ef90cdc943445bdca9f8ed1087f
Starlitriver1234 <C24745999@mytudublin.ie>
2025-05-15 13:56:00 +0100

    UI progress

--- godottest/Scripts/game.gd
+++ godottest/Scripts/game.gd
@@ -3,6 +3,7 @@ extends Node2D
 var lives = 5
 var enemy_number = 0
 @onready var number = $Enemy_Number
+@onready var Lives = $Lives
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,12 +18,15 @@ func _process(delta: float) -> void:
 func _on_spawner_enemy_spawned(EnemyInstance: Variant) -> void:
 	add_child(EnemyInstance)
 	enemy_number += 1
-	_set_score_label(enemy_number)
+	_set_enemy_label(enemy_number)
 
 
-func _set_score_label(enemy_number):
+func _set_enemy_label(enemy_number):
 	number.text = "Enemies: " + str(enemy_number)
 
 func _on_hurt(lives: Variant) -> void:
 	lives -= 1
-	print(lives)
+	_set_lives_label(lives)
+	
+func _set_lives_label(lives):
+	Lives.text = "Lives: " + str(lives)
