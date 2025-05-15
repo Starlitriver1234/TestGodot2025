cb4964ee6e54656f07608752cea4e822559748ae
Starlitriver1234 <C24745999@mytudublin.ie>
2025-05-15 14:37:19 +0100

    added a game over scene

--- godottest/Scripts/game.gd
+++ godottest/Scripts/game.gd
@@ -6,12 +6,9 @@ var enemy_number = 0
 @onready var number = $Enemy_Number
 @onready var Lives = $Lives
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
